01283102add1281a67f4104e6bb485a4c1d30090
guenka <guenkadesouza123@gmail.com>
2022-11-23 22:05:07 -0300

    teste

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Projeto_professor_daniel.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
